f4bba2b15a8af9af7a9e1754b7791920400efcc5
Madhu Reddy <madhu@scientiamobile.com>
2021-06-10 23:24:02 -0400

    added subprocess run file

---
+++ os-and-subprocess-module/subprocess-run-grep-file.py
@@ -0,0 +1,9 @@
+import subprocess
+#result will be bytes format (as we have not used text=True, capture_output is used to store output in a variable
+output=subprocess.run(["ls", "-l"], capture_output=True)
+
+#input parameter takes input from the stdout of above command (we need to use "stdin" instead of "input", when input is from a file or provided  at runtime)
+filter=subprocess.run(["grep", "mylearningsguru.pdf"], input=output.stdout, capture_output=True)
+
+#the "filter" variable contains stdout in bytes format, so we need to decode it to str format for better visibility
+print(filter.stdout.decode('utf-8'))
